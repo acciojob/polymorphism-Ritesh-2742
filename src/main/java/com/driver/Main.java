0c50037f7f2a8b9298769f0e406465fcf2ed8128
Ritesh-2742 <rpoudiyal@gmail.com>
2022-12-12 12:45:11 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,22 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        Product p= new Product();
-        p.product(10,20);
-        p.product(10,20,30);
-        p.product(10.1,20.3);
 
-
-
-    }
     public static class Product{
-
-        public static int product(int x, int y) {
+        public int product(int x , int y){
             return x*y;
         }
-        public static int product(int x, int y, int z) {
+        public int product(int x , int y, int z){
             return x*y*z;
         }
-        public double product(double x, double y) {
-            return x*y*z;
+        public double product(double x , double y){
+            return x*y;
         }
-
     }
-
+    public static void main(String args[]){
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2,1.4);
+    }
 }
